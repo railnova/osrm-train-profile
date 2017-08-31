44013b228d74e879b419d72783f8d14b3057a57b
Pierre Gerard <piegerar@ulb.ac.be>
2017-08-31 15:15:17 +0200

    [fix] syntax error

--- train-fret.lua
+++ train-fret.lua
@@ -30,8 +30,8 @@ function way_function(way, result)
     name = way:get_value_by_key("name"),
     ref = way:get_value_by_key("ref"),
     maxspeed = way:get_value_by_key("maxspeed"),
-    oneway = way:get_value_by_key("oneway")
-    highspeed = way:get_value_by_key("highspeed") -- unused, could be used to exclude freight
+    oneway = way:get_value_by_key("oneway"),
+    highspeed = way:get_value_by_key("highspeed"),
   }
 
   if (
